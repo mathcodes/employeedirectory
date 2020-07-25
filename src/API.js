d68f676d132b8e4670b23a788e43c1c150f7bca5
Jon Christie <jonpchristie@gmail.com>
2020-07-25 04:54:30 -0400

    large overhaul, huge learning experience

--- src/API.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const getUsers = function() {
-  return axios.get("https://randomuser.me/api/?results=100&nat=us");
-};
--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-import React, { useEffect, useState } from "react";
-import FilterInput from "./FilterInput";
-import Table from "./Table";
-import { getUsers } from "./API";
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+
 import "./App.css";
 
 function App() {
-  const [initialUsers, updateAvailableUsers] = useState([]);
-  const [usersToRender, updateUsersToRender] = useState([]);
-
-  useEffect(() => {
-    getUsers().then(({ data: { results } }) => updateAvailableUsers(results));
-  }, []);
-
-
-
   return (
     <div className="App">
-      <h1 className="ED">Employee Directory</h1>
-      <p>
-        To filter by first name, please begin your search in the user input
-        below
-      </p>
-      <FilterInput users={initialUsers} updateUsers={updateUsersToRender} />
-      <Table users={usersToRender} />
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
     </div>
   );
 }
 
-
-
 export default App;
+
