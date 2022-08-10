36a6753c9d87469e3e053a483f7d877eda8af056
Jon Christie <jonchristie@Jons-MBP.attlocal.net>
2022-08-10 04:51:52 -0400

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "../styles/Header.css";
 function Header () {
     return (
       <div className="header">
-        <img src="./blueJcircle.png" alt="blueJ logo" className="logo" />
+        <img className="logoNav" src="https://raw.githubusercontent.com/mathcodes/employeedirectory/master/src/components/blueJcircle.png" alt="blueJ logo" width="60px"/>
         <h1>Employee Directory</h1>
         <p></p>
       </div>
